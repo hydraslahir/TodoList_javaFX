2312962e6cc076888b30eb1cd05a65a60969d84b
hydraslahir <tommy3667@live.ca>
2020-09-20 18:00:29 -0400

    initialisation du git

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,18 +1,21 @@
-package sample;
+package Controller;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Todo List application");
+
+        BorderPane root = new BorderPane();
+
+        Scene scene = new Scene(root,400,400, Color.WHITE);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
